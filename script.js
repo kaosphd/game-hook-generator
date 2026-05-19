dd8194621c1eb6e6ea05f71d0c5afbc156119fa7
kaosphd <kaos.phd@gmail.com>
2026-05-19 07:37:12 +0100

    Update script.js
    CSV per sheet update

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 // ==========================================
-// PASTE YOUR GOOGLE SHEET ID HERE
+// GOOGLE SHEET CSV URLS
 // ==========================================
 
-const SHEET_ID = "PASTE_YOUR_SHEET_ID_HERE";
+const sheetURLs = {
+
+  Where:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=0&single=true&output=csv",
+
+  When:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1712047057&single=true&output=csv",
+
+  Why:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=700876301&single=true&output=csv",
+
+    How:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1945825130&single=true&output=csv",
+
+    Who:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1635706100&single=true&output=csv",
+
+    WorkingIn:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1819449839&single=true&output=csv",
+
+   What:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=793795044&single=true&output=csv",
+  
+  Adjective:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1874611889&single=true&output=csv"
+};
 
 // ==========================================
-// FETCH TAB DATA
+// FETCH SHEET DATA
 // ==========================================
 
 async function fetchSheet(tabName) {
 
-  const url =
-    `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&sheet=${tabName}`;
-
-  const response = await fetch(url);
+  const response = await fetch(sheetURLs[tabName]);
 
   const data = await response.text();
 
