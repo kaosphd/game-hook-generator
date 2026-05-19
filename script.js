0ddcb5fc010361c92a964c95d5fef7f10ba76cb3
kaosphd <kaos.phd@gmail.com>
2026-05-19 07:58:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,28 +4,28 @@
 
 const sheetURLs = {
 
-  Who:
+  "Who":
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1635706100&single=true&output=csv",
 
-  Where:
+  "Where":
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=0&single=true&output=csv",
 
-  When:
+  "When":
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1712047057&single=true&output=csv",
 
-  What:
+  "What":
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=793795044&single=true&output=csv",
 
-  Why:
+  "Why":
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=700876301&single=true&output=csv",
 
-  How:
+  "How":
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1945825130&single=true&output=csv",
 
-  WorkingIn:
+  "WorkingIn":
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1819449839&single=true&output=csv",
 
-  Adjective:
+  "Adjective":
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1874611889&single=true&output=csv"
 };
 
