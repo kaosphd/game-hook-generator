dda322620a80cb8b01acfeca306512fe1059adea
kaosphd <kaos.phd@gmail.com>
2026-05-19 07:45:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,27 +4,28 @@
 
 const sheetURLs = {
 
+  You are:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1635706100&single=true&output=csv",
+  
   Where:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=0&single=true&output=csv",
 
   When:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1712047057&single=true&output=csv",
 
+     Trying to:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=793795044&single=true&output=csv",
+
   Why:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=700876301&single=true&output=csv",
 
-    How:
+    By doing:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1945825130&single=true&output=csv",
 
-    Who:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1635706100&single=true&output=csv",
-
-    WorkingIn:
+Working as:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1819449839&single=true&output=csv",
 
-   What:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=793795044&single=true&output=csv",
-  
+
   Adjective:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn_5zWPlvOCNffXkC7cq1GtUt-inijCocyStwl8WCeZzt7Riei15QdUE1BfM1_6tf4nTMx9xMBpZII/pub?gid=1874611889&single=true&output=csv"
 };
